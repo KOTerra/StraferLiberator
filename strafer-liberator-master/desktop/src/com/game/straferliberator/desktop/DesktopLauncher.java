4de549515454d5252fbc7c301ff5f611dcc5a58c
KOTerra <82443072+KOTerra@users.noreply.github.com>
2022-03-22 14:08:24 +0200

    initial commit

---
+++ strafer-liberator-master/desktop/src/com/game/straferliberator/desktop/DesktopLauncher.java
@@ -0,0 +1,18 @@
+package com.game.straferliberator.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.game.straferliberator.StraferLiberator;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title = "Strafer Liberator";
+		config.width = 1920;
+		config.height = 1080;
+		//config.fullscreen=true;
+		config.pauseWhenMinimized=true;
+		config.resizable=false;
+		new LwjglApplication(new StraferLiberator(), config);
+	}
+}
