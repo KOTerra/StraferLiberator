ad7cfcbe22029e5cf552d403a59ea5c7245192d5
KOTerra <82443072+KOTerra@users.noreply.github.com>
2022-03-24 13:58:57 +0200

    added package to work on port

--- strafer-liberator-master/desktop/src/com/game/straferliberator/desktop/DesktopLauncher.java
+++ strafer-liberator-master/desktop/src/com/game/straferliberator/desktop/DesktopLauncher.java
@@ -8,8 +8,8 @@ public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		config.title = "Strafer Liberator";
-		config.width = 1920;
-		config.height = 1080;
+		config.width = 1024;
+		config.height = 576;
 		//config.fullscreen=true;
 		config.pauseWhenMinimized=true;
 		config.resizable=false;
