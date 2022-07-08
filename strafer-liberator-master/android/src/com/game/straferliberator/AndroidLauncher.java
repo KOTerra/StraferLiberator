420595f06eab13b9b968a357ac5b0752ce9eff5f
KOTerra <82443072+KOTerra@users.noreply.github.com>
2022-07-08 15:36:07 +0300

    added support for notch displays #15

--- strafer-liberator-master/android/src/com/game/straferliberator/AndroidLauncher.java
+++ strafer-liberator-master/android/src/com/game/straferliberator/AndroidLauncher.java
@@ -1,8 +1,12 @@
 package com.game.straferliberator;
 
 import android.content.res.Resources;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
+import android.view.Window;
+import android.view.WindowManager;
+
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
@@ -17,14 +21,21 @@ public class AndroidLauncher extends AndroidApplication {
 	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 
-
 		WorldData.runOnAndroid=true;
+
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+			Window applicationWindow = getApplicationWindow();
+			WindowManager.LayoutParams attrib = applicationWindow.getAttributes();
+			attrib.layoutInDisplayCutoutMode = WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_SHORT_EDGES;
+		}
+
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		config.useImmersiveMode=true;
+
 		DisplayMetrics screenDimensions= Resources.getSystem().getDisplayMetrics();
 		WorldData.setResolution(screenDimensions.widthPixels, screenDimensions.heightPixels);
 
 
-		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
-		config.useImmersiveMode=true;
 		initialize(new StraferLiberator(), config);
 	}
 }
